4cc756abb9aac2c35f368db598b2bcecd10c43db
Alexander Georgievskiy <galeksandrp@gmail.com>
2023-11-19 11:42:27 +0300

    v1

---
+++ Phoenix/Binaries/Win64/Mods/NoBroomFlightWindEffects/Scripts/main.lua
@@ -0,0 +1,30 @@
+--Author: yakuzadeso
+
+function NoBroomFlightWindEffects()
+	local broomflightwind = StaticFindObject(
+		"/Game/VFX/Mechanics/Broom/BP_BroomFlightVFXActor.BP_BroomFlightVFXActor_C:PostProcess_GEN_VARIABLE")
+
+	if not broomflightwind:IsValid() then
+		ExecuteWithDelay(2000, NoBroomFlightWindEffects)
+		return
+	end
+	print("NoBroomFlightWindEffects is On!")
+	broomflightwind.bVisible = false
+end
+
+local shouldInitTheMod = false
+
+NotifyOnNewObject("/Script/Phoenix.Loadingcreen", function(self)
+	shouldInitTheMod = true
+	print("NoBroomFlightWindEffects is starting up!")
+end)
+
+RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(Context, NewPawn)
+	if shouldInitTheMod then
+		shouldInitTheMod = false
+
+		print("NoBroomFlightWindEffects hooked on load/reload succesfully!")
+
+		NoBroomFlightWindEffects()
+	end
+end)
